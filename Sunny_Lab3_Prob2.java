835c22e599e3a0d2ddd2cfaed2097c202f7adcb5
Sara Sunny <110349622+ssunny264@users.noreply.github.com>
2026-05-10 12:29:50 -0400

    Create Sunny_Lab3_Prob2 class with main method
    Initial implementation of the Sunny_Lab3_Prob2 class with main method.

---
+++ Sunny_Lab3_Prob2.java
@@ -0,0 +1,25 @@
+/*
+ * Sara Sunny
+ * Lab 3 - Problem 2 - Main
+ */
+package sunny_lab3_prob2;
+
+/**
+ *
+ * @author slbru
+ */
+public class Sunny_Lab3_Prob2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        ReadFindDeviation myFile = new ReadFindDeviation();
+
+        myFile.Read();
+        myFile.Deviation();
+
+        System.out.println();
+    }
+    
+}
